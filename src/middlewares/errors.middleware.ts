8f3d40df6749236cd6e67f99b9f92850a81e0ff5
Leo Araujo <leonardo_02452@hotmail.com>
2022-07-30 22:12:15 -0300

    feat: middleware que lida com os erros da app

---
+++ src/middlewares/errors.middleware.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from 'express';
+
+const errorMiddleware = (err: Error, _req: Request, res: Response, _next: NextFunction) => {
+  const { name, message } = err;
+  switch (name) {
+    case 'ValidationError':
+      return res.status(400).json({ message });
+    case 'UnauthorizedError':
+      return res.status(401).json({ message });
+    default:
+      return res.sendStatus(500);
+  }
+};
+
+export default errorMiddleware;
