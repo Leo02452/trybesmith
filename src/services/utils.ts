fb00ae81525d0fbfd04857a3ec8055c1cef27352
Leo Araujo <leonardo_02452@hotmail.com>
2022-08-01 18:51:39 -0300

    perf: diminuição de complexidade
    Ao trocar a condição (if) de verificação do tipo de erro, a
    complexidade da função foi reduzida e não foi necessaria função auxiliar

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -7,23 +7,15 @@ export class CustomError extends Error {
   }
 }
 
-const verifyTypeAndThrowError = (type: string, message: string, errorName: string) => {
-  switch (type) {
-    case 'string.base':
-    case 'string.min':
-    case 'number.base':
-    case 'number.min':
-      throw new CustomError(message, 'UnprocessableEntityError');
-    default:
-      throw new CustomError(message, errorName);
-  }
-};
-
 export const runSchema = (schema: Schema) => (data: unknown): void => {
   const { error } = schema.validate(data);
   if (error) {
     const { type, message } = error.details[0];
-    
-    verifyTypeAndThrowError(type, message, error.name);
+    switch (type) {
+      case 'any.required':
+        throw new CustomError(message, error.name);
+      default:
+        throw new CustomError(message, 'UnprocessableEntityError');
+    }
   }
 };
