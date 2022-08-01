f6209e05638e17a12d0827cfe96695ac44516443
Leo Araujo <leonardo_02452@hotmail.com>
2022-08-01 18:56:19 -0300

    feat: add validação de body para cadastrar pedido

--- src/services/orders.service.ts
+++ src/services/orders.service.ts
@@ -1,12 +1,24 @@
+import Joi from 'joi';
 import connection from '../models/connection';
 import OrderModel from '../models/order.model';
 import OrderInterface from '../interfaces/order.interface';
+import { runSchema } from './utils';
 
 export default class OrdersService {
   public model: OrderModel;
 
+  public runSchema;
+
   constructor() {
     this.model = new OrderModel(connection);
+    this.runSchema = runSchema;
+  }
+
+  public validateBody(unknown: unknown) {
+    this.runSchema(Joi.object({
+      productsIds: Joi.array().items(Joi.number().required()).required()
+        .messages({ 'array.includesRequiredUnknowns': '"productsIds" must include only numbers' }),
+    }))(unknown);
   }
 
   public async getAll(): Promise<OrderInterface[]> {
